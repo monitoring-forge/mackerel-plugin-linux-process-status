058998825b59e464babe6b78b603f1cc312d707b
Masahiro Nagano <m-nagano@sakura.ad.jp>
2026-07-29 20:44:09 +0900

    refactor again

--- main.go
+++ main.go
@@ -207,13 +207,8 @@ func main() {
 
 func _main() int {
 	opt := &Opt{}
-	psr := flags.NewParser(opt, flags.HelpFlag|flags.PrintErrors|flags.PassDoubleDash)
+	psr := flags.NewParser(opt, flags.HelpFlag|flags.PassDoubleDash)
 	_, err := psr.Parse()
-	if flags.WroteHelp(err) {
-		return OK
-	} else if err != nil {
-		return UNKNOWN
-	}
 	if opt.Version {
 		if commit == "" {
 			commit = "dev"
@@ -227,6 +222,12 @@ func _main() int {
 			runtime.Version(),
 			commit)
 		return OK
+	} else if flags.WroteHelp(err) {
+		fmt.Fprintf(os.Stdout, "%v\n", err)
+		return OK
+	} else if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return UNKNOWN
 	}
 
 	err = opt.run()
